129bf1a284463841aa807343ff4db152ab233e38
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-03-14 17:53:58 -0300

    update database

--- Services/PedidoService.cs
+++ Services/PedidoService.cs
@@ -9,7 +9,7 @@ public class PedidoService
         return new List<Pedido>
         {
             new Pedido { Nome = "Brendon Berzins", Tamanhos = new List<string> { "M" }, Pago = 45, TotalAPagar = 45 },
-            new Pedido { Nome = "Emilly", Tamanhos = new List<string> { "M", "M" }, Pago = 0, TotalAPagar = 90 },
+            new Pedido { Nome = "Emilly", Tamanhos = new List<string> { "M", "M" }, Pago = 90, TotalAPagar = 90 },
             new Pedido { Nome = "Tânia Regina", Tamanhos = new List<string> { "GG" }, Pago = 45, TotalAPagar = 45 },
             new Pedido { Nome = "Jaqueline", Tamanhos = new List<string> { "M" }, Pago = 45, TotalAPagar = 45 },
             new Pedido { Nome = "Marcelo", Tamanhos = new List<string> { "M" }, Pago = 45, TotalAPagar = 45 },
@@ -19,7 +19,7 @@ public class PedidoService
             new Pedido { Nome = "Paulo", Tamanhos = new List<string> { "GG", "GG" }, Pago = 90, TotalAPagar = 90 },
             new Pedido { Nome = "Ingrid", Tamanhos = new List<string> { "G" }, Pago = 45, TotalAPagar = 45 },
             new Pedido { Nome = "Geovana", Tamanhos = new List<string> { "BL" }, Pago = 45, TotalAPagar = 45 },
-            new Pedido { Nome = "Bruno Emanuel", Tamanhos = new List<string> { "P", "P" }, Pago = 0, TotalAPagar = 45 },
+            new Pedido { Nome = "Bruno Emanuel", Tamanhos = new List<string> { "P", "P" }, Pago = 0, TotalAPagar = 90 },
             new Pedido { Nome = "Irene", Tamanhos = new List<string> { "PP" }, Pago = 0, TotalAPagar = 45 },
             new Pedido { Nome = "Josiane", Tamanhos = new List<string> { "BL" }, Pago = 45, TotalAPagar = 45 },
             new Pedido { Nome = "Ane Maria", Tamanhos = new List<string> { "G" }, Pago = 45, TotalAPagar = 45 },
